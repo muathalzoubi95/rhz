78a51056cf2a6f5c78dbd90671672b3a1bc64bf8
Muath A. Zoubi <moathaza@gmail.com>
2024-10-22 11:59:45 +0300

    Update site.ts
    Hi

--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,7 @@ export type SiteConfig = typeof siteConfig
 export const siteConfig = {
   name: "muath.js",
   description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+    " This app is not ready to use i just test it ",
   mainNav: [
     {
       title: "Home",
@@ -11,9 +11,9 @@ export const siteConfig = {
     },
   ],
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
+    twitter: "#",
+    github: "#",
+    docs: "#",
   },
   technologies:[
     "JavaScript",
