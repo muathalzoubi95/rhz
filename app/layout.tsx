532bd4597b8cc978fe055c82d18d64e0e00e2a28
Muath K Alzoubi <moathaza@gmail.com>
2026-08-24 01:16:39 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,6 +41,30 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <body
           className="min-h-screen bg-background font-sans antialiased"
         >
+<Script id="twipla-tracking" strategy="afterInteractive">
+  {`
+    (function(v,i,s,a,t){
+      v[t]=v[t]||function(){
+        (v[t].v=v[t].v||[]).push(arguments)
+      };
+      if(!v._visaSettings){
+        v._visaSettings={}
+      }
+      v._visaSettings[a]={
+        v:'1.0',
+        s:a,
+        a:'1',
+        t:t
+      };
+      var b=i.getElementsByTagName('body')[0];
+      var p=i.createElement('script');
+      p.defer=1;
+      p.async=1;
+      p.src=s+'?s='+a;
+      b.appendChild(p)
+    })(window,document,'//app-worker.visitor-analytics.io/main.js','e8b5c467-9f3f-11f1-9aa5-960004340fd3','va')
+  `}
+</Script>
           <Analytics/>
           <CrispChat/>
           <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
